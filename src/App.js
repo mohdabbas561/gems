1f99f8e06e0a8688ffd7a5c01e2bc87ef2c0dce1
Mohd Abbas <mohammadabbas561@gmail.com>
2024-12-14 21:50:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { FaTwitter, FaTelegram } from "react-icons/fa";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {/* Navbar */}
+      <nav className="navbar">
+        <div className="logo">
+          $GME<span>STOP</span>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <a href="#">NFT</a>
+          </li>
+          <li>
+            <a href="#">Casino</a>
+          </li>
+          <li>
+            <a href="#">Whitepaper</a>
+          </li>
+        </ul>
+        <div className="social-icons">
+          <FaTwitter className="icon" />
+          <FaTelegram className="icon" />
+        </div>
+      </nav>
+
+      {/* Hero Section */}
+      <header className="hero">
+        <h1>
+          Wall Street Can't Stop Us <span> Coming on $NEAR</span>
+        </h1>
+        <button className="join-button">Stay Tuned!</button>
       </header>
     </div>
   );
